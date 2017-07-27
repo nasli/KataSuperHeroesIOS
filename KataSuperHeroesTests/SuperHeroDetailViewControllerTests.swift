f3ee31b0ab22b96ce47b5d8b7196902dd300a3da
nasli <nalvarez@solidgear.es>
2017-07-27 17:22:31 +0200

    Add tests

--- KataSuperHeroesTests/SuperHeroDetailViewControllerTests.swift
+++ KataSuperHeroesTests/SuperHeroDetailViewControllerTests.swift
@@ -6,4 +6,91 @@
 //  Copyright © 2017 GoKarumi. All rights reserved.
 //
 
+//
+//  SuperHeroDetailViewControllerTests.swift
+//  KataSuperHeroes
+//
+//  Created by Pedro Vicente Gomez on 13/01/16.
+//  Copyright © 2016 GoKarumi. All rights reserved.
+//
+
 import Foundation
+import KIF
+import Nimble
+import UIKit
+@testable import KataSuperHeroes
+
+class SuperHeroDetailViewControllerTests: AcceptanceTestCase {
+    
+    fileprivate let repository = MockSuperHeroesRepository()
+    
+    func testShowsSuperHeroNameAsTitle() {
+        let superHeroe = givenASuperHeroWithName()
+        
+        openSuperHeroDetailViewController(superHeroe.name)
+        
+        tester().waitForView(withAccessibilityLabel: superHeroe.name)
+        
+    }
+    
+    func testShowsSuperHeroName() {
+        let superHero = givenASuperHeroWithName()
+        
+        openSuperHeroDetailViewController(superHero.name)
+        
+        tester().waitForView(withAccessibilityLabel: "Name: \(superHero.name)")
+    }
+    
+    func testShowsSuperHeroDescription() {
+        let superHero = givenASuperHeroWithName()
+        
+        openSuperHeroDetailViewController(superHero.name)
+        
+        tester().waitForView(withAccessibilityLabel: "Description: \(superHero.name)")
+        
+    }
+    
+    func testDoesNotShowAvengersBadgeIfTheHeroIsNotPartOfTheAvengersTeam() {
+        let superHero = givenASuperHeroWithName()
+        
+        openSuperHeroDetailViewController(superHero.name)
+        tester().waitForAbsenceOfView(withAccessibilityLabel: "Avengers Badge")
+    }
+    
+    func testShowsAvengersBadgeIfTheSuperHeroIsPartOfTheAvengersTeam() {
+        let superHero = givenASuperHeroWithName(true)
+        
+        openSuperHeroDetailViewController(superHero.name)
+        tester().waitForView(withAccessibilityLabel: "Avengers Badge")
+    }
+    
+    func testDoesNotShowLoadingViewOnceSuperHeroHasBeenLoaded() {
+ 
+        let superHero = givenASuperHeroWithName()
+        
+        openSuperHeroDetailViewController(superHero.name)
+        
+        tester().waitForAbsenceOfView(withAccessibilityLabel: "Loading View")
+    }
+    
+    fileprivate func givenASuperHeroWithName(_ isAvenger: Bool = false) -> SuperHero {
+        let superHero = SuperHero(name: "Mr. Clean",
+                                  photo: URL(string: "https://i.annihil.us/u/prod/marvel/i/mg/c/60/55b6a28ef24fa.jpg"),
+                                  isAvenger: isAvenger, description: "Description")
+        repository.superHeroes = [superHero]
+        return superHero
+    }
+    
+    fileprivate func openSuperHeroDetailViewController(_ superHeroName: String) {
+        let superHeroDetailViewController = ServiceLocator()
+            .provideSuperHeroDetailViewController(superHeroName) as! SuperHeroDetailViewController
+        superHeroDetailViewController.presenter = SuperHeroDetailPresenter(ui: superHeroDetailViewController,
+                                                                           superHeroName: superHeroName,
+                                                                           getSuperHeroByName: GetSuperHeroByName(repository: repository))
+        let rootViewController = UINavigationController()
+        rootViewController.viewControllers = [superHeroDetailViewController]
+        present(viewController: rootViewController)
+        tester().waitForAnimationsToFinish()
+    }
+}
+
--- KataSuperHeroesTests/SuperHeroesViewControllerTests.swift
+++ KataSuperHeroesTests/SuperHeroesViewControllerTests.swift
@@ -15,7 +15,7 @@ import UIKit
 class SuperHeroesViewControllerTests: AcceptanceTestCase {
 
     fileprivate let repository = MockSuperHeroesRepository()
-
+ 
     func testShowsEmptyCaseIfThereAreNoSuperHeroes() {
         givenThereAreNoSuperHeroes()
 
@@ -23,7 +23,113 @@ class SuperHeroesViewControllerTests: AcceptanceTestCase {
 
         tester().waitForView(withAccessibilityLabel: "¯\\_(ツ)_/¯")
     }
+    
+    func testNoShowsEmptyCaseIfThereAreSuperHeroes() {
+        givenThereAreSomeSuperHeroes(6, avengers: false)
+        
+        openSuperHeroesViewController()
+        
+        tester().waitForAbsenceOfView(withAccessibilityLabel: "¯\\_(ツ)_/¯")
+    }
+    
+    func testShowsExactSuperHeroesGivenSomeSuperHeroes() {
+        givenThereAreSomeSuperHeroes(6, avengers: false)
+        
+        openSuperHeroesViewController()
+        
+        let tableView = tester().waitForView(withAccessibilityLabel: "SuperHeroesTableView") as! UITableView
+        
+        expect(tableView.numberOfRows(inSection: 0)).to(equal(6))
+    }
+    
+    func testNoShowsLoadingWhenWeHaveData() {
+        givenThereAreSomeSuperHeroes(6, avengers: false)
+        
+        openSuperHeroesViewController()
+        
+        let tableView = tester().waitForView(withAccessibilityLabel: "SuperHeroesTableView") as! UITableView
+        if tableView.numberOfRows(inSection: 0) == 0 {
+            tester().waitForView(withAccessibilityLabel: "loading")
+        } else {
+            tester().waitForAbsenceOfView(withAccessibilityLabel: "loading")
+        }
+    }
+    
+    func testShowsThatTheHeroesInTheListHasTheCorrectName () {
+        let superHeros = givenThereAreSomeSuperHeroes()
+        
+        openSuperHeroesViewController()
+        
+        for superHero in superHeros {
+            tester().waitForView(withAccessibilityLabel: superHero.name)
+        }
+    }
+    
+    func testShowsThatTheHeroesInTheListAreAvengers () {
+        let superHeroes = givenThereAreSomeSuperHeroes(10, avengers: true)
+        
+        openSuperHeroesViewController()
+        
+        for superHero in superHeroes {
+            
+            tester().waitForView(withAccessibilityLabel:"\(superHero.name) - Avengers Badge")
+        }
+    }
+    
+    func testShowsThatTheHeroesInTheListAreNotAvengers () {
+        let superHeroes = givenThereAreSomeSuperHeroes(10, avengers: false)
+        
+        openSuperHeroesViewController()
+        
+        for superHero in superHeroes {
+            
+            tester().waitForAbsenceOfView(withAccessibilityLabel:"\(superHero.name) - Avengers Badge")
+        }
+    }
+    
+    func testShowsThatTheEvenHeroesInTheListAreNotAvengers () {
+        let superHeroes = givenThereAreSomeSuperHeroes(10, avengers: true)
+        
+        openSuperHeroesViewController()
+        
+        var evens: [SuperHero] = []
+        for i in 0...superHeroes.count-1 where i % 2 == 0 {
+            evens.append(superHeroes[i])
+        }
+        
+        for superHero in evens {
+            tester().waitForView(withAccessibilityLabel: "\(superHero.name) - Avengers Badge")
+        }
+    }
+    
+    func testShowsThatTheOddHeroesInTheListAreNotAvengers () {
+        let superHeroes = givenThereAreSomeSuperHeroes(10, avengers: true)
+        
+        openSuperHeroesViewController()
+        
+        var odds: [SuperHero] = []
+        for i in 0...superHeroes.count-1 where i % 2 == 1 {
+            odds.append(superHeroes[i])
+        }
+        
+        for superHero in odds {
+            tester().waitForView(withAccessibilityLabel: "\(superHero.name) - Avengers Badge")
+        }
+    }
 
+    func testNavigateToDetailSuperHeroes() {
+        let superheroes = givenThereAreSomeSuperHeroes(10, avengers: true)
+        
+        openSuperHeroesViewController()
+
+        let tableView = tester().waitForView(withAccessibilityLabel: "SuperHeroesTableView") as! UITableView
+        
+        let indexPath = IndexPath(item: 0, section: 0)
+        
+        tester().tapRow(at: indexPath, in: tableView)
+        tester().waitForView(withAccessibilityLabel: superheroes[0].name)
+    }
+    
     fileprivate func givenThereAreNoSuperHeroes() {
         _ = givenThereAreSomeSuperHeroes(0)
     }
