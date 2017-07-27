e9bff633bd9f379757f5863f39c9230717c66a50
nasli <nalvarez@solidgear.es>
2017-07-27 17:20:53 +0200

    Add test tableView and detail

---
+++ KataSuperHeroesTests/SuperHeroDetailViewControllerTests.swift
@@ -0,0 +1,9 @@
+//
+//  SuperHeroDetailViewControllerTests.swift
+//  KataSuperHeroes
+//
+//  Created by Noelia Alvarez on 27/07/2017.
+//  Copyright © 2017 GoKarumi. All rights reserved.
+//
+
+import Foundation
